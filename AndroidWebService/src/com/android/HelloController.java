2127d31ff9e55db0aead7283002db6c05233c027
rajeevshukla <mail2rajeevshukla@gmail.com>
2014-08-12 12:08:12 +0530

    this is first commit

---
+++ AndroidWebService/src/com/android/HelloController.java
@@ -0,0 +1,9 @@
+package com.android;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+	
+}
---
+++ AndroidWebService/src/com/android/web/MainController.java
@@ -0,0 +1,8 @@
+package com.android.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+}
