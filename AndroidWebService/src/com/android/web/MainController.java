69a3e0c8e631457df6f8f23745da48c23a8b6227
rajeevshukla <mail2rajeevshukla@gmail.com>
2014-08-25 21:22:43 +0530

    added some more controller

--- AndroidWebService/src/com/android/web/MainController.java
+++ AndroidWebService/src/com/android/web/MainController.java
@@ -1,8 +1,23 @@
 package com.android.web;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
+	
+	@RequestMapping(value="welcome.htm")
+	public ModelAndView welcome(HttpServletResponse response){
+		
+		 ModelAndView mav=new ModelAndView("welcome");
+		 
+		 return mav;
+		
+	}
+	
+	
 }
